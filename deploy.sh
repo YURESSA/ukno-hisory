704e5f06c2f61fe27f18c37d3e41e140fc83d92c
YURESSA <goshrenko@gmail.com>
2026-05-22 16:04:39 +0500

    chore: harden docker deployment and JWT config

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -eu
+
+BRANCH="${1:-branch-for-deploy}"
+PROJECT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
+cd "$PROJECT_DIR"
+
+git fetch origin
+git checkout "$BRANCH"
+git reset --hard "origin/$BRANCH"
+
+docker compose up --build -d --remove-orphans
+docker image prune -f
